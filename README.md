176dd01ba65894426e4c232a122f6acfc7c67042
Lakshay Bhala <bhalal@cardiff.ac.uk>
2018-11-20 10:11:52 +0000

    Add login form

---
+++ README.md
@@ -0,0 +1,3 @@
+# backonline-project-team-6
+
+Client project for the CM6112 Introduction to Web Development module.
---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+var pwd = document.getElementById('password');
+var eye = document.getElementById('eye');
+
+eye.addEventListener('click', togglePass);
+
+function togglePass() {
+  eye.classList.toggle('active');
+  (pwd.type == 'password') ? pwd.type='text' : pwd.type='password';
+}
+
+$('#age').keyup(function() {
+  $('#age').val(this.value.match(/^[0-9]{1,3}$/));
+});
