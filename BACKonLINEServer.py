18baffe608a43499ae0933e755b6acb9e2214289
Sepehr Naseri <naseris@cardiff.ac.uk>
2018-11-23 03:08:22 +0000

    Server can now get answers to show from the database on the html page

--- BACKonLINEServer.py
+++ BACKonLINEServer.py
@@ -2,54 +2,34 @@ import os
 from flask import Flask, redirect, request, render_template, make_response, escape, session
 import sqlite3
 
-DATABASE = 'database.db'
-
+DATABASE = 'BackonLine.db'
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-counter = 1
 
 
 @app.route("/Questions", methods = ['POST','GET'])
-def questions():
-
+def studentAddDetails():
+	# if request.method =='GET':
+	# 	return render_template('Questions.html')
 	if request.method =='GET':
-		global counter
 		try:
+			questionid = '1'#request.form.get('name', default="Error") #rem: args for get form for post
 			conn = sqlite3.connect(DATABASE)
 			cur = conn.cursor()
-			cur.execute("SELECT QuestionText FROM Questions WHERE QuestionID=?;",[counter])
+			# cur.execute("SELECT * FROM Students WHERE surname=? AND public = 'True';", [surname])
+			#AND  public = 'True'
+			cur.execute("SELECT * FROM Options WHERE QuestionID=? ;", [questionid])
 			data = cur.fetchall()
-			conn.close()
+			print(data)
 		except:
 			print('there was an error', data)
 			conn.close()
 		finally:
-			data = str(data)[3:-4]
 			conn.close()
-
-			counter+=1
+			#return str(data)
 			return render_template('Questions.html', data = data)
-
-	if request.method =='POST':
-		OptionID = request.form.get('OptionID', default="Error")#rem: args for get form for post
-		QuestionID = request.form.get('QuestionID', default="Error")
-#		PatientID = request.form.get('PatientID', default="Error" to be added after login made
-		print("inserting student"+OptionID)
-		try:
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("INSERT INTO Response ('OptionID',)\
-						VALUES (?,?)",(OptionID, QuestionID) ) #Patient ID to be added
-			conn.commit()
-			msg = "Record successfully added"
-		except:
-			conn.rollback()
-			msg = "error in insert operation"
-		finally:
-			conn.close()
-			return msg
 
 @app.route("/index")
 def customerDetailsForm():
@@ -65,32 +45,34 @@ def customerAddDetails():
 # Sessions
 
 # Cookies login
-@app.route("/Login", methods = ['GET','POST'])
-def login():
-    if request.method=='POST':
-        uName = request.form.get('username', default="Error")
-        if checkCredentials(uName, pw):
-            resp = make_response(render_template('Customer.html', msg='hello '+uName, username = uName))
-            resp.set_cookie('username', uName)
-            if uName=="Ian":
-                user_type = "admin"
-                resp.set_cookie('username', uName, 'user-type', user_type)
-            else:
-                user_type = "customer"
-                resp.set_cookie('username', uName, 'user-type', user_type)
-        else:
-            resp = make_response(render_template('Customer.html', msg='Incorrect  login',username='Guest'))
-        return resp
-    else:  # if it is a GET
-        username = request.cookies.get('username')
-        return render_template('index.html', msg='', username = username)
+# @app.route("/Login", methods = ['GET','POST'])
+# def login():
+#     if request.method=='POST':
+#         uName = request.form.get('username', default="Error")
+#         if checkCredentials(uName, pw):
+#             resp = make_response(render_template('Customer.html', msg='hello '+uName, username = uName))
+#             resp.set_cookie('username', uName)
+#             if uName=="Ian":
+#                 user_type = "admin"
+#                 resp.set_cookie('username', uName, 'user-type', user_type)
+#             else:
+#                 user_type = "customer"
+#                 resp.set_cookie('username', uName, 'user-type', user_type)
+#         else:
+#             resp = make_response(render_template('Customer.html', msg='Incorrect  login',username='Guest'))
+#         return resp
+#     else:  # if it is a GET
+#         username = request.cookies.get('username')
+#         return render_template('index.html', msg='', username = username)
+#
+#
+# #       methods
+# def checkCredentials(uName, pw):
+#     return pw == 'ian'
+#
+# # =======================================================================
+# #      the db creation methods
 
-
-#       methods
-def checkCredentials(uName, pw):
-    return pw == 'ian'
-
-
-#This is important it allows the server to run don't delete
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
+	#app.run(host='0.0.0.0', port=8080)
