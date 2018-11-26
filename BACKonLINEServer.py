99f15a33c3b0d60cee961e1af28d73db7b676a4d
Lakshay Bhala <bhalal@cardiff.ac.uk>
2018-11-26 16:36:59 +0000

    Encrypt password and navbar changes

--- BACKonLINEServer.py
+++ BACKonLINEServer.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, redirect, request, render_template, make_response, escape, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_mail import Mail, Message
 import sqlite3
 
@@ -20,6 +21,10 @@ mail = Mail(app)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 counter = 1
+
+@app.route("/test", methods = ['GET'])
+def test():
+    return render_template('test2.html',username="foo")
 
 @app.route("/Questions", methods = ['POST', 'GET'])
 def questions():
@@ -79,26 +84,26 @@ def welcomepage():
 app.secret_key = 'fj590Rt?h40gg'
 
 # Cookie sessions
-@app.route("/Login", methods = ['GET','POST'])
+@app.route("/Login", methods = ['GET', 'POST'])
 def login():
-    if request.method=='POST':
+    if request.method == 'POST':
         reminder ="***** REM other pages WILL NOT be able to access the username as they are not set up to use Cookie Sessions *****"
         sign_name = request.form.get('name', default="Error")
         sign_gender = request.form.get('gender', default="Error")
         sign_age = request.form.get('age', default="Error")
         sign_email = request.form.get('email-signup', default="Error")
         sign_password = request.form.get('email-password', default="Error")
-
+        sign_password = generate_password_hash(sign_password)
         login_email = request.form.get('email-login', default="Error")
         login_password = request.form.get('password', default="Error")
-        if sign_name =="":
-            print("logging in")
+        if sign_name == "":
+            print("Logging in")
             if checkCredentials(login_email, login_password):
                 resp = make_response(render_template('welcome.html', msg='Hello '+login_email+reminder, username=login_email))
             else:
-                resp = make_response(render_template('index.html', msg='', login_email = login_email, error = "Incorect Login"))
-        if sign_name !="":
-            print("signing in")
+                resp = make_response(render_template('index.html', msg='', login_email = login_email, error="Incorect login"))
+        if sign_name != "":
+            print("Signing in")
             try:
                 conn = sqlite3.connect(DATABASE)
                 cur = conn.cursor()
@@ -108,7 +113,7 @@ def login():
                 print("Record successfully added")
             except:
                 conn.rollback()
-                print("error in insert operation")
+                print("Error in insert operation")
             resp = make_response(render_template('welcome.html', msg='Hello '+sign_email+reminder, username=sign_email))
         print(f"name: {sign_name}, gender: {sign_gender}, age: {sign_age}, username: {sign_email}, password: {sign_password}")
         return resp
@@ -116,19 +121,18 @@ def login():
         username = 'none'
         if 'username' in session:
             username = escape(session['username'])
-        return render_template('index.html', msg='', username = username, error = "")
+        return render_template('index.html', msg='', username=username, error="")
 
 # =================Methods================================
 def checkCredentials(email, password):
     try:
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
-        cur.execute("SELECT email,password FROM Patient WHERE email=?;", [email])
+        cur.execute("SELECT email, password FROM Patient WHERE email=?;", [email])
         login_details = cur.fetchall()
     except:
         print('There was an error', login_details)
-
-    if email == login_details[0][0] and password == login_details[0][1]:
+    if email == login_details[0][0] and check_password_hash(login_details[0][1], password):
         return True
     else:
         return False
