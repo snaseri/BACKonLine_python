ad1557263d6c5e58642e5608e98d37d318065dbb
Lakshay Bhala <bhalal@cardiff.ac.uk>
2018-11-27 13:49:32 +0000

    Move js code to js file

--- static/js/questions.js
+++ static/js/questions.js
@@ -27,3 +27,39 @@ $('h1').each(function() {
     $(this).html(ques);
   };
 });
+
+function back() {
+  console.log("Back button");
+  var direction = document.getElementById("direction").value;
+  document.getElementById("direction").value = "back"
+  var calculation = document.getElementById("questnum").value;
+  var counter = document.getElementById("counter").innerHTML;
+  console.log(direction);
+
+  if (counter == calculation && counter !=1 ){
+    document.getElementById("questnum").value -= 1
+  };
+  if (calculation-counter == 1){
+    document.getElementById("questnum").value = calculation - 2;
+  };
+};
+
+function forward() {
+  console.log("Next button");
+  var direction = document.getElementById("direction").value;
+  document.getElementById("direction").value = "forward";
+  var calculation = parseInt(document.getElementById("questnum").value);
+  var counter = document.getElementById("counter").innerHTML;
+  console.log("Back button--");
+  var direction = document.getElementById("direction").value;
+  document.getElementById("direction").value = "forward";
+  var calculation = parseInt(document.getElementById("questnum").value);
+  var counter = document.getElementById("counter").innerHTML;
+  if (counter == calculation){
+    document.getElementById("questnum").value = calculation + 1;
+  };
+  if (counter-calculation == 1){
+    document.getElementById("questnum").value = calculation + 2;
+  };
+  console.log(direction);
+};
